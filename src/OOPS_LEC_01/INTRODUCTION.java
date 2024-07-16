2768a3371c3f017c08d1c0656acc8f3aedd0d08f
Yogesh <longquest.oyscommunity@gmail.com>
2024-07-16 23:04:50 +0530

    CLASS_INTRO

--- src/OOPS_LEC_01/INTRODUCTION.java
+++ src/OOPS_LEC_01/INTRODUCTION.java
@@ -1,7 +1,34 @@
+// HOW EXACTLY A CLASS WORKS....
+
 package OOPS_LEC_01;
 
 public class INTRODUCTION {
     public static void main(String[] args) {
-        System.out.println("hello");
+        student print = new student(9,88);
+        student p = new student(10);
+        System.out.println(print.a);
+        System.out.println(print.b);
+        p.jj();
     }
 }
+class student{ //by default constructor function will be the name of the class
+    int a ;
+    int b ;
+
+    void jj(){
+        System.out.println("the age is "+a);
+    }
+    student(int u){
+        this.a=u;//used to update the value inside the class during run time
+    }
+    student(int a,int b){
+        this.b = b;
+        this.a = a;
+    }
+}
+
+//01 IF YOU DON'T USE ANY CONSTRUCTOR WITH ATTRIBUTE JAVA WILL TAKE THE CONSTRUCTOR AS DEFAULT FROM CLASS NAME
+//02 DECLARING A DATATYPE INSIDE THE CLASS WILL HELP YOU TO USE A DEFAULT VALUE ANYWHERE INSIDE THE CLASS (line:15,16)
+//03 TO CHANGE THE OBJECT OF THE TEMPLATE WHILE RUN TIME "THIS" KEYWORD IS USED
+//04 FOR ALL METHOD USED INSIDE THE CLASS NEEDS NEW OBJECT CREATION IN MAIN (ex:(method jj()) line:9)
+//05 YOU CAN USE THE CONSTRUCTORS VALUES IN AN FUNCTION (line:22 in line:19 print by line:11)
