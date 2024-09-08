c2652227f8ad154bc833ce2b84e5b78e9b8969bd
Yogesh <longquest.oyscommunity@gmail.com>
2024-09-08 14:03:52 +0530

    08/09/24

--- src/BASIC_INTRO.java
+++ src/BASIC_INTRO.java
@@ -10,7 +10,7 @@ public class BASIC_INTRO {
 
 
 //        1.what these will help is us to enter the element one by one
-//        BUT WHT IF THEY ASK US TO ENTER THE 5 DATA OF STUDENT (not elements)
+//        BUT WHAT IF THEY ASK US TO ENTER THE 5 DATA OF STUDENT (not elements)
 
 //        1st thought is
         int[] b = new int[2];
@@ -39,24 +39,103 @@ public class BASIC_INTRO {
 
 //        HOW CAN I ACCESS AND USE MY TEMPLATE
 
-//        dot operator plays a vital role in the class and object concept
+//----------------------------------------------------------------------------DEFAULT CONSTRUCTOR----------------------------------------------------------------------------//
 
         Student access = new Student();
-        access.name = "yogesh";
-        access.age = 19;
         System.out.println(Arrays.toString(student));
         System.out.println(access.name);
         System.out.println(access.age);
-        System.out.println();
+        System.out.println(access.roll);
+        access.age = 16;
+        System.out.println(access.age);
+        access.prin();
+        Student access2 = new Student();
+        access2.prin();
 
+//----------------------------------------------------------------------------PARAMETERIZED CONSTRUCTOR----------------------------------------------------------------------------//
+
+//        O1 IS FOR INITIALIZING THE VALUE WITHOUT USING this KEYWORD
+        Student o1 = new Student(12,"hello",17);
+//        what it does is that it gets the value ,but it returns the declaration value it does not change the value Since we used the same variable name as we used for declaration in the parameter
+        System.out.println(o1.roll);
+        System.out.println(o1.name);
+        System.out.println(o1.age);
+//        O2 WITH USING this KEYWORD
+        Student o2 = new Student("hello",14,18);
+        System.out.println(o2.name);
+        System.out.println(o2.roll);
+        System.out.println(o2.age);
+//        03 WITH DIFFERENT NAME THAN WE DECLARE
+        Student o3 = new Student(12,15,"its o3");
+        System.out.println(o3.age);
+        System.out.println(o3.roll);
+        System.out.println(o3.name);
+//        O4 OBJECT AS PARAMETER
+        Student o4 = new Student(o3);
+        System.out.println(o4.name);
+        System.out.println(o4.age);
+        System.out.println(o4.roll);
     }
 }
+
+//---------------------------------------------------------------------------------CLASS STUDENT----------------------------------------------------------------------------//
 
 class Student{
     int roll;
     String name ;
     int age;
+
+//----------------------------------------------------------------------------DEFAULT CONSTRUCTOR----------------------------------------------------------------------------//
+
     Student(){
-        System.out.println("hello");
+//        In the constructor you can initialize or add a value like you add in the main using object , but you cannot use the object name ...since the
+//        objet name is for the template of class you are about to initialize...
+//        EXAMPLE OF YOU CAN'T :
+
+//        access.roll = 12;
+//        access.name = "yogesh";
+//        access.age  = 15;
+
+//        but what you can do is you can initialize for the template using "this" keyword
+
+        this.roll = 12;
+        this.name = "yogesh"; //output in 46,47,48
+        this.age  = 15; //you can also change the initialized value using dot ( line 49 )
+
+//        where this will be usefull ( prin() ) useful for different object with different name
     }
+    void prin(){
+        System.out.println("hello my name is "+ name); //In line 51 I printed the method using existing object ,In 53 with a new object but still it gave me the same answer because of the initialization
+    }
+
+//----------------------------------------------------------------------------PARAMETERIZED CONSTRUCTOR----------------------------------------------------------------------------//
+
+//    TO INITIALIZE FOR DIFFERENT OBJECT WITH DIFFERENT VALUES YOU NEED TO ADD PARAMETERS AND ARGUMENTS
+//    o1 and o2 are the objects used for this with same variable name used in parameter like declare
+    Student(int roll,String name, int age){
+        roll = roll;
+        name = name;
+        age = age;
+    }
+    Student(String name,int roll, int age){
+        this.roll = roll;
+        this.name = name;
+        this.age = age;
+    }
+//    o3 object used to initialize with different name in the parameter
+    Student(int rol, int ge,String ame){
+        roll = rol;
+        name = ame;
+        age = ge;
+    }
+
+//  YOU CAN PASS A WHOLE OBJECT INTO A CONSTRUCTOR AS A PARAMETER ()
+
+    Student(Student access3){
+        this.name = access3.name;
+        this.age = access3.age;
+        this.roll = access3.roll;
+    }
+
+
 }
